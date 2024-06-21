8a4fa194f3c58cacfb7fbea6fbede1b50b9cb28f
reagan02 <r.mabawad.529147@umindanao.edu.ph>
2024-06-21 15:18:33 +0800

    update

--- src/Components/Navigation/NavBar.jsx
+++ src/Components/Navigation/NavBar.jsx
@@ -14,7 +14,8 @@ const NavBar = () => {
 		<div className="sticky top-0 z-10 bg-white ">
 			<nav className="flex justify-between items-center py-2 font-bold relative ">
 				<a className="md:text-lg lg:text-xl">Portfolio</a>
-				<button className="md:hidden">
+
+				<button className="md:hidden ml-auto">
 					{showMobileMenu ? (
 						<>
 							<HiOutlineXMark

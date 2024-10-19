6704d0064f6d64f866a56beecf3437cb9db5e448
reagan13 <reaganmabawad@gmail.com>
2024-10-20 01:59:22 +0800

    change email

--- src/Components/Sections/GetInTouch.jsx
+++ src/Components/Sections/GetInTouch.jsx
@@ -38,7 +38,7 @@ const GetInTouch = () => {
 					please reach out. I look forward to connecting with you!
 				</p>
 				<p className="text-sm sm:text-base md:text-xl font-bold ">
-					r.mabawad.529147@umindanao.edu.ph
+					reaganmabawad13@gmail.com
 				</p>
 				<p className="md:text-xl font-bold">+63 9455419799</p>
 			</div>

1d6033c8282032d0dd7536f1d14f8a6cb88d97bd
reagan13 <reaganmabawad@gmail.com>
2025-03-17 00:33:10 +0800

    change experience order

--- src/data/workexperience.js
+++ src/data/workexperience.js
@@ -2,15 +2,6 @@ import mediaone from "../assets/mediaone.png";
 import codeability from "../assets/codebility.jpg";
 export const experience = [
 	{
-		logoSrc: mediaone,
-		logoAlt: "MediaOne Logo",
-		position: "Back End Developer | Internship",
-		company: "MediaOne Software Solutions",
-		dateRange: "June 2024 - July 2024",
-		description:
-			"I worked as a backend developer on a team that built dynamic forms from scratch based on our client preferences. I designed the database, set up the backend, and created APIs that connect everything. Working closely with my team, I helped create a system that lets our client easily build and manage forms, making their experience smooth and efficient.",
-	},
-	{
 		logoSrc: codeability,
 		logoAlt: "Codeability Logo",
 		position: "Full-Stack Developer | Internship",
@@ -18,5 +9,14 @@ export const experience = [
 		dateRange: "October 2024 - February 2025",
 		description:
 			"I contributed to various side projects while gaining hands-on experience in full-stack software development. I collaborated on coding tasks independently, focusing on enhancing my technical skills across both frontend and backend technologies. I engaged in project-based learning where I applied my knowledge to real-world challenges, helping me grow as a developer.",
+	},
+	{
+		logoSrc: mediaone,
+		logoAlt: "MediaOne Logo",
+		position: "Back End Developer | Internship",
+		company: "MediaOne Software Solutions",
+		dateRange: "June 2024 - July 2024",
+		description:
+			"I worked as a backend developer on a team that built dynamic forms from scratch based on our client preferences. I designed the database, set up the backend, and created APIs that connect everything. Working closely with my team, I helped create a system that lets our client easily build and manage forms, making their experience smooth and efficient.",
 	},
 ];
